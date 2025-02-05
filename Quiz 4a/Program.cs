64f26b397479f045ee330c6c13adb14eba911675
Jose Villa <villa1997.jv@gmail.com>
2025-02-05 13:20:08 -0600

    Add project files.

---
+++ Quiz 4a/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_4a
+{
+    internal class Program
+    {
+        static long Factorial(int aNumber)
+        {
+            //Create method to calculate factorial
+            long retval = Int32.MinValue;
+
+            long product = 1;
+
+            for (int i = 1; i <= aNumber; i++)
+            {
+                product += i;
+            }
+            retval = product;
+            return retval;
+        }
+        static void Main(string[] args)
+        {
+            //declarations
+
+            Console.WriteLine(Factorial(20));
+
+
+            Console.Read();
+
+
+
+
+
+
+        }
+    }
+}
